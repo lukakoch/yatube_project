d3d41656b25d3a18005da2f4c3041372d0057406
kocher <lukakoh@yandex.ru>
2022-05-17 03:27:32 +0300

    Создание структуры проекта

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    return render(request, template)
 
 def group_posts(request, slug):
     return HttpResponse(f'Пост {slug}')
