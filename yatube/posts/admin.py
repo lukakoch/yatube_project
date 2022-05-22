8bcb6b2723ff190abe43c040e030e999d0eb7fd5
kocher <lukakoh@yandex.ru>
2022-05-22 20:06:48 +0300

    Проект готов

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Group, Post
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    # Перечисляем поля, которые должны отображаться в админке
+    list_display = (
+        'pk',
+        'text', 
+        'pub_date', 
+        'author',
+        'group',
+    ) 
+    # Добавляем интерфейс для поиска по тексту постов
+    list_editable = ('group',)
+    search_fields = ('text',) 
+    list_filter = ('pub_date',) 
+    empty_value_display = '-пусто-'
+# При регистрации модели Post источником конфигурации для неё назначаем
+# класс PostAdmin
+admin.site.register(Post, PostAdmin) 
+admin.site.register(Group)
--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,10 +1,12 @@
 # posts/urls.py
+from argparse import Namespace
 from django.urls import path
 
 from . import views
+app_name = 'posts'
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name = 'index'),
 
-    path('group/<slug:slug>/', views.group_posts),
+    path('group<slug:slug>', views.group_posts, name = 'group_list'),
 ]
